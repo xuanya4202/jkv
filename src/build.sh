9f851cd5573631640595044796cc25e53db8d54a
xuanya4202 <haojinglong4202@163.com>
2018-12-10 05:28:35 -0500

    modified

---
+++ src/build.sh
@@ -0,0 +1,7 @@
+cd ..
+curdir=`pwd`
+export GOPATH=$curdir
+export CGO_CFLAGS="-I ${curdir}/src/vendor/rocksdb/include"
+export CGO_LDFLAGS="-L${curdir}/src/vendor/rocksdb -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -lzstd"
+cd src/main
+go build
